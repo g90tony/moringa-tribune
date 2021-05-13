6db1f8e2d6c0c3274c8d6652f5314e04fb77cf54
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 00:32:34 +0300

    added pasted news url route

--- news/urls.py
+++ news/urls.py
@@ -7,4 +7,5 @@ app_name = 'news'
 urlpatterns = [
     path('', views.welcome, name='index'),
     path('/today', views.news_of_day, name='Todays News'),
+    re_path(r'/past-news/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews')
 ]
