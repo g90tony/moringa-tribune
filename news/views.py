f648e3340ff468bbf68d33916d8c48a4f623f8cd
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 02:48:46 +0300

    removed convert date
    changed output from httpresponse to render

--- news/views.py
+++ news/views.py
@@ -1,38 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, Http404
 import datetime as dt 
 
-def convert_date(day, month):
-
-    
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    months = ['January, February', 'March', 'April', 'May', 'June', 'July' ,'August', 'September', 'October', 'November', 'December']
-    
-    day_str = days[day]
-    month_str = months[month]
-    
-    return {'day': day_str, 'month': month_str}
 
 # Create your views here.
 def welcome(request):
     
-    return HttpResponse('Welcome to the Moringa Tribune')    
+    return render(request, 'welcome.html', {"title": "Home: Moringa Tribune"})  
 
 
 def news_of_day(request):
-    today = dt.date.today()
+    today = dt.date.today() 
     
-    converted = convert_date(today.weekday(), today.month -2)
-    
-    markup = f''' 
-                <html>
-                    <body>
-                        <h1>{converted['day']} - {converted['month']} - {today.year} </h1>
-                    </body>
-                </html>
-              '''
-              
-    return HttpResponse(markup)
+    return render(request, 'all-news/todays-news.html', {'date': today,})
 
 
 def past_days_news(request, past_date):
@@ -42,15 +22,10 @@ def past_days_news(request, past_date):
         
     except ValueError:
         raise Http404()
-
-    converted = convert_date(query_date.weekday(), query_date.month)
-    
-    markup = f''' 
-            <html>
-                <body>
-                    <h1>{converted['day']} {query_date.strftime("%d")} {converted['month']} {query_date.year} </h1>
-                </body>
-            </html>
-            '''
+        assert False
+        
+    if query_date == dt.date.today():
+        return redirect(news_of_day)
+        
             
-    return HttpResponse(markup)
+    return render(request, 'all-news/past-news.html', {'date': query_date})
