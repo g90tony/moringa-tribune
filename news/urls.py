325f62d7efa160d614bc0bf395ee7b775553baa9
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 16:01:09 +0300

    imported image upload settings from app/settings.py to added a generated url path

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,7 @@
+from os import stat
 from django.urls import path, re_path, register_converter
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,3 +11,6 @@ urlpatterns = [
     re_path('^$', views.news_of_day, name='Todays News'),
     re_path(r'^past-news/(?P<past_date>[0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.past_days_news, name='Past News')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
