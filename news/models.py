650a95b807251e8dca09c830de33d36c5c7bbbee
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 05:11:32 +0300

    added phone number property

--- news/models.py
+++ news/models.py
@@ -6,18 +6,21 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
     
     def __str__(self):
         return self.first_name
     
     class Meta:
         ordering = ['first_name']
+    
         
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     
     def __str__(self):
         return self.name
+    
     
 class Article(models.Model):
     title = models.CharField(max_length=60)
