95ea97059dfe90b4aec5d1b061e132e9e05ea63a
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 04:59:03 +0300

    added the editor model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+    
+    class Meta:
+        ordering = ['first_name']
+        
