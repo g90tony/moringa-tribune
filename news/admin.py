6d4b18612679a38289fb2e5ea47d3583530f229b
Caleb Tony <calebmbugua@gmail.com>
2021-05-14 05:25:40 +0300

    added tags ordering

--- news/admin.py
+++ news/admin.py
@@ -3,6 +3,9 @@ from .models import Editor, Article, Tag
 
 # Register your models here.
 
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags')
+
 admin.site.register(Editor)
 admin.site.register(Article)
 admin.site.register(Tag)
