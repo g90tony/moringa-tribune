d0561644b73c89fad2d219b98f5f504d75fadb4a
Caleb Tony <calebmbugua@gmail.com>
2021-05-15 08:34:01 +0300

    added search path

--- news/urls.py
+++ news/urls.py
@@ -9,6 +9,7 @@ app_name = 'news'
 
 urlpatterns = [
     re_path('^$', views.news_of_day, name='Todays News'),
+    re_path('^search/', views.search_news, name='Search Results'),
     re_path(r'^past-news/(?P<past_date>[0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.past_days_news, name='Past News')
 ]
 
