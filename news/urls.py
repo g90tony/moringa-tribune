82bb6157de1f0448e8e292f755382c27cc465d41
Caleb Tony <calebmbugua@gmail.com>
2021-05-19 13:27:57 +0300

    revised the view article path

--- news/urls.py
+++ news/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     re_path('^$', views.news_of_day, name='Todays News'),
     re_path('^search/', views.search_news, name='Search Results'),
     re_path(r'^past-news/(?P<past_date>[0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.past_days_news, name='Past News'),
-    path(r'^article/<int:article_id>$', views.article, name='View Article')
+    path(r'^article/(?P<article_id>[0-9])/$', views.article, name='View Article')
 ]
 
 if settings.DEBUG:
