0935f4fc2896d02bde6d7463979c933fcf0f6f8b
Caleb Tony <calebmbugua@gmail.com>
2021-05-20 11:55:43 +0300

    removed editor model
    set editor to the user model

--- news/models.py
+++ news/models.py
@@ -1,24 +1,8 @@
 from datetime import date
 from django.db import models
 from cloudinary.models import CloudinaryField
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=10, blank=True)
-    
-    def __str__(self):
-        return self.first_name
-    
-    def save_editor(self):
-        self.save()
-    
-    class Meta:
-        ordering = ['first_name']
-    
-        
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     
@@ -29,7 +13,7 @@ class Tag(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    editor = models.ForeignKey(User, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_img = CloudinaryField('image', default=None)
